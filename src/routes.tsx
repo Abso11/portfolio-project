db7ea6dd976aac7652be89750ddd14e9c1a259dc
Abso11 <yankes107@gmail.com>
2023-04-24 13:37:09 +0200

    feat: setup unit tests

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,17 @@
-import { useParams } from 'react-router';
+// import { useParams } from 'react-router';
 import { BrowserRouter, Routes as ReactRoutes, Route, Navigate } from 'react-router-dom';
 import MainLayout from 'components/main-layout';
 import { Dashboard } from 'views';
 import { UserDetails } from 'views/user-details';
 
-type PagePathTypes = {
-  pagePath: string;
-};
+// type PagePathTypes = {
+//   pagePath: string;
+// };
 
-const RedirectToDetailsPage = ({ pagePath }: PagePathTypes): JSX.Element => {
-  const { id } = useParams();
-  return <Navigate to={pagePath.replace(':id', id as string)} />;
-};
+// const RedirectToDetailsPage = ({ pagePath }: PagePathTypes): JSX.Element => {
+//   const { id } = useParams();
+//   return <Navigate to={pagePath.replace(':id', id as string)} />;
+// };
 
 const Routes = (): JSX.Element => (
   <BrowserRouter>
